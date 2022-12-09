ca73817ba4ed0ff667f2595c241cbba6ab00bfb0
jstinson144 <112037078+jstinson144@users.noreply.github.com>
2022-12-09 17:23:49 -0600

    Removed unused code and made catch print out an exception was thrown

--- FlightReservation/src/ProgressBarLoader.java
+++ FlightReservation/src/ProgressBarLoader.java
@@ -1,6 +1,4 @@
-// This class creates the progress bar and prints to the console
-
-//import java.awt.Color;
+// This class creates the progress bar 
 
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
@@ -17,8 +15,6 @@ public class ProgressBarLoader {
         bar.setSize(200, 20);
         bar.setBounds(0, 0, 420, 80);
         bar.setStringPainted(true);
-        // bar.setBackground(Color.black);
-        // bar.setForeground(Color.green);
 
         frame.add(bar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,12 +32,10 @@ public class ProgressBarLoader {
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-
+            	System.out.println("Thread sleep threw an exception");
             }
             counter += Math.random() * 12;
         }
-        // bar.setString("Done! :)");
         counter = 0;
         frame.setVisible(false);
     }
